c419e000a45453d0f9f7ba888436e103d5465c46
Adam Pash <adam.pash@gmail.com>
2015-02-03 14:43:43 -0500

    can link to specific post

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  resource :posts do
-    resources :comments
-  end
   get '/posts/:post_id/comments' => 'comments#index'
+  get '/posts/:id' => 'posts#show'
 
-  # get '/stories/:kinja_id' => 'stories#show'
   root "posts#index"
 end
