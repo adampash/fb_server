5093808bf060aab15461401d812f412222b455fc
Adam Pash <adam.pash@gmail.com>
2015-02-03 18:04:07 -0500

    Extracted user/pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "binders", password: "women" if ENV["RAILS_ENV"] == "production"
+  http_basic_authenticate_with name: ENV["basic_name"], password: ENV["basic_password"] if ENV["RAILS_ENV"] == "production"
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
