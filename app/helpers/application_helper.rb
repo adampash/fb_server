f87347f9f13ea539501177c004149124d7b10490
Adam Pash <adam.pash@gmail.com>
2015-02-04 09:56:59 -0500

    Helper for text formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def prepare_text(message, query)
+    simple_format(highlight(auto_link(message, :html => {target: '_blank'}) {|text| truncate(text, :length => 55)}, query), class: 'message')
+  end
 end
