4e5f110a72bbae6f203d6063ebe2f465e2204e31
Adam Pash <adam.pash@gmail.com>
2015-02-18 13:47:58 -0500

    Update for mobile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def prepare_text(message, query)
-    simple_format(highlight(auto_link(message, :html => {target: '_blank'}) {|text| truncate(text, :length => 55)}, query), class: 'message')
+    simple_format(highlight(auto_link(message, :html => {target: '_blank'}) {|text| truncate(text, :length => 40)}, query), class: 'message')
   end
 end
